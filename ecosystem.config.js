3c821c50e439a7d2e837e753ddb15090739b9a06
Phamzito <phamzito@hotmail.com>
2020-06-04 10:36:00 -0500

    añadi PM2

---
+++ ecosystem.config.js
@@ -0,0 +1,10 @@
+/* eslint-disable @typescript-eslint/camelcase */
+module.exports = {
+    apps: [
+        {
+            name: 'Husky',
+            script: './dist/index.js',
+            watch: false,
+        },
+    ],
+};
